12d65e4463b6a030229a3f1b601315c5feb24de3
24K DESIGNS <kalvin.kao@hotmail.com>
2024-09-21 00:41:39 -0400

    update b4a slider

--- scripts.js
+++ scripts.js
@@ -103,3 +103,16 @@ document.addEventListener('keydown', function(event) {
         moveSlide(1);
     }
 });
+
+
+
+
+
+////////////////////////////////////////////////
+$("#slider").on("input change", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.foreground-img').css('width', `${sliderPos}%`)
+    // Update the position of the slider button
+    $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+  });
